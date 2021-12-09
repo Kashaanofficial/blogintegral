28cc3e33499463047dd44c9e6119a21febe97aa0
Kashaanofficial <95303666+Kashaanofficial@users.noreply.github.com>
2021-12-09 20:57:40 +0530

    Delete Integraluniversity.js

--- Integraluniversity.js
+++
@@ -1,9 +0,0 @@
-<script>
-    (function(){
-        var t = document.getElementsByTagName("script")[0];
-        var s = document.createElement("script"); s.async = true;
-        s.src = "//integration.nfusionsolutions.biz/client/jh/widget/module/accordionchart/nfaccordion";
-        t.parentNode.insertBefore(s, t);
-    })();
-    </script>
-    
